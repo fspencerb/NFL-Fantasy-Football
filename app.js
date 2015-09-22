279f5544fea67b4d2dce37c3cdd9a7a7ed388ecd
Forrest Spencer Buster <fspencerb@gmail.com>
2015-09-22 11:00:19 -0600

    fixed bugs

--- app.js
+++ app.js
@@ -1,168 +1,182 @@
-var PlayersService = function(endpointUri){
-	
-	var playersData = [];
-​
-	
-	this.getPlayersByTeam = function(teamName){
-		var filteredPlayers = playersData.filter(function(player){
-			return player.team === teamName;
-		});
-	
-		return filteredPlayers;
-	}
-​
-	
-	this.getPlayersByPosition = function(position){
-		var filteredPlayers = playersData.filter(function(player){
-			return player.position === position;
-		});
-​
-		return filteredPlayers;
-	}
-	
-	function loadPlayersData(){
-		$.getJSON(endpointUri,function(data){
-			playersData = data.body.players; //maybe play with this
-		});
-	};	
-	loadPlayersData();
-}
-​
-//consumer
-//service already linked in HTML
-var prodUrl = "cbs.com....";
-var testUrl = "test.cbs.com....";
-​
-var playerService = new PlayerService(testUrl);
-​
-$('some-button').on('click',function(){
-	var sf = playerService.getPlayersByTeam("SF");
-}
+// var PlayersService = function(endpointUri){
+// 	
+// 	var playersData = [];
+// ​
+// 	
+// 	this.getPlayersByTeam = function(teamName){
+// 		var filteredPlayers = playersData.filter(function(player){
+// 			return player.team === teamName;
+// 		});
+// 	
+// 		return filteredPlayers;
+// 	}
+// ​
+// 	
+// 	this.getPlayersByPosition = function(position){
+// 		var filteredPlayers = playersData.filter(function(player){
+// 			return player.position === position;
+// 		});
+// ​
+// 		return filteredPlayers;
+// 	}
+// 	
+// 	function loadPlayersData(){
+// 		$.getJSON(endpointUri,function(data){
+// 			playersData = data.body.players; //maybe play with this
+// 		});
+// 	};	
+// 	loadPlayersData();
+// }
+// ​
+// //consumer
+// //service already linked in HTML
+// var prodUrl = "cbs.com....";
+// var testUrl = "test.cbs.com....";
+// ​
+// var playerService = new PlayerService(testUrl);
+// ​
+// $('some-button').on('click',function(){
+// 	var sf = playerService.getPlayersByTeam("SF");
+// }
+// 
+// var roster = {
+//   players:{},
+//   addPlayer :function(player){
+//     if(player.name && player.position && player.number){
+//       this.players[player.id] = player;
+//       updatePlayers();
+//     }else{
+//       alert("Invalid Player Data");
+//     }
+//   }
+// }
+// ​
+// var Player = function(name,position,number,id){
+//   this.name = name;
+//   this.position = position;
+//   this.number = number;
+//   this.id = id;
+// }
+// ​
+// ​
+// var PlayerFactory = {
+//   _uniqueId:0,
+//   createPlayer:function(name,position,number){
+//     this._uniqueId++;
+//         return new Player(name,position,number,this._uniqueId);
+//   }
+// }
+// ​
+// ​
+// function updatePlayers(){
+//   var rosterDiv = $(".player-roster");
+//   rosterDiv.html('');
+//   for(var id in roster.players){
+//       var player = roster.players[id];
+//       //without the if statement this loop throws an error
+//       if(!player){
+//         //continue;
+//       }
+//       var html = '<div class="player-card">'+
+//             '<button class="btn btn-xs btn-danger remove" id="'+player.id+'">Remove</button>'+
+//             '<div>'+
+//               '<img src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/" />'+
+//             '</div>'+
+//             '<div>'+
+//               '<span>'+player.name+'</span>'+
+//             '</div>'+
+//             '<div>'+
+//               '<span>'+player.position+'</span>'+
+//             '</div>'+
+//             '<div>'+
+//               '<span>'+player.number+'</span>'+
+//            '</div>'+
+//           '</div>';
+//           
+//       rosterDiv.append(html);
+//   }
+// }
+// ​
+// $(function(){
+//   
+//   $("#add-player-form").on('submit',function(event){
+//       event.preventDefault();
+//       var values = $(this).serializeArray();
+//       var name = values[0].value;
+//       var position = values[1].value;
+//       var number = values[2].value;
+//       roster.addPlayer(PlayerFactory.createPlayer(name,position,number));
+//   });
+//   
+//   $(".expand-player-panel").on('click',function(){
+//     $(".panel-add-player .panel-body").toggle(200)
+//   });
+//   
+//   $(".player-roster").on('click','.remove',function(event){
+//     delete roster.players[this.id];
+//     updatePlayers();
+//   })
+//     
+//   
+// })
 
-var roster = {
-  players:{},
-  addPlayer :function(player){
-    if(player.name && player.position && player.number){
-      this.players[player.id] = player;
-      updatePlayers();
-    }else{
-      alert("Invalid Player Data");
+var playerService = function () {
+  var _players = [];
+  return {
+    loadPlayers: function (cb) {
+      var url = "http://bcw-getter.herokuapp.com/?url=";
+      var url2 = "http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=football&response_format=json";
+      var apiUrl = url + encodeURIComponent(url2);
+      $.getJSON(apiUrl, function (response) {
+        _players = response.body.players;
+        console.log(_players);
+        cb();
+      });
+    },
+    getPlayers: function () {
+      return _players;
+    },
+    getPlayersByTeam: function (team) {
+      var requestedTeam = _players.filter(function (player) {
+        if (player.pro_team === team) {
+          return true;
+        }
+      });
+      return requestedTeam;
     }
   }
 }
-​
-var Player = function(name,position,number,id){
-  this.name = name;
-  this.position = position;
-  this.number = number;
-  this.id = id;
-}
-​
-​
-var PlayerFactory = {
-  _uniqueId:0,
-  createPlayer:function(name,position,number){
-    this._uniqueId++;
-        return new Player(name,position,number,this._uniqueId);
-  }
-}
-​
-​
-function updatePlayers(){
-  var rosterDiv = $(".player-roster");
-  rosterDiv.html('');
-  for(var id in roster.players){
-      var player = roster.players[id];
-      //without the if statement this loop throws an error
-      if(!player){
-        //continue;
-      }
-      var html = '<div class="player-card">'+
-            '<button class="btn btn-xs btn-danger remove" id="'+player.id+'">Remove</button>'+
-            '<div>'+
-              '<img src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/" />'+
-            '</div>'+
-            '<div>'+
-              '<span>'+player.name+'</span>'+
-            '</div>'+
-            '<div>'+
-              '<span>'+player.position+'</span>'+
-            '</div>'+
-            '<div>'+
-              '<span>'+player.number+'</span>'+
-           '</div>'+
-          '</div>';
-          
-      rosterDiv.append(html);
-  }
-}
-​
-$(function(){
-  
-  $("#add-player-form").on('submit',function(event){
-      event.preventDefault();
-      var values = $(this).serializeArray();
-      var name = values[0].value;
-      var position = values[1].value;
-      var number = values[2].value;
-      roster.addPlayer(PlayerFactory.createPlayer(name,position,number));
-  });
-  
-  $(".expand-player-panel").on('click',function(){
-    $(".panel-add-player .panel-body").toggle(200)
-  });
-  
-  $(".player-roster").on('click','.remove',function(event){
-    delete roster.players[this.id];
-    updatePlayers();
-  })
-    
-  
-})
 
-        $('#addPlayer').on('click',function(){
-            
-            console.log("hi")
-    	    var name = $('#playerName').val();
-    	    var position = $('#playerPosition').val();
-    	    var number = $('#playerNumber').val();
-     
-     
-    	var html = '<div class="player-card">'+
-			  '<div>'+
-              '<div style="text-align:center"><span type="button" class="btn btn-danger btn-xs" id="removePlayer">Remove</span></div>'+
-                '<image src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/"></image>'+
-              '</div>'+
-			  '<div>'+
-				'<span style="text-align:center">' + (name) + '</span>' +
-			  '</div>'+
-			  '<div>'+
-				'<span style="text-align:center">' + (position) + '</span>' +
-			  '</div>'+
-			  '<div>'+
-				'<span style="text-align:center">' + (number) + '</span>' +
-			  '</div>'+
-			'</div>';
-    	$('.player-roster').append(html);
-        });
+var ps = playerService();
+
+ps.loadPlayers(function () {
+ console.log('request done')
+});
+$('#addPlayer').on('click', function (e) {
+  e.preventDefault();
+  var name = $('#playerName').val();
+  var position = $('#playerPosition').val();
+  var num = $('#playerNumber').val();
+
+  var html = '<div class="player-card">' +
+    '<button type="button" class="btn btn-danger btn-xs remove-player">Remove</button>' +
+    '<img src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/"/>' +
+    '<div>' +
+    '<span style="text-align:center">' + name + '</span>' +
+    '</div>' +
+    '<div>' +
+    '<span style="text-align:center">' + position + '</span>' +
+    '</div>' +
+    '<div>' +
+    '<span style="text-align:center">' + num + '</span>' +
+    '</div></div>';
+  $('.player-roster').append(html);
+});
 
 
-        $("#removePlayer").on('click',function(){
+$(".player-roster").on('click', '.remove-player', function (e) {
+  e.preventDefault();
+  var removebtn = $(this);
+  removebtn.parent().remove();
+});
 
-           var html = '<div class="player-card">' +
-               '<div>' +
-                  '<div style="text-align:center"><button type="button" class="btn btn-danger btn-xs" id="removePlayer">Remove</button></div>' +
-               '<image src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/"></image>' +
-          '</div>' +
-          '<div>' +
-            '<span>' + (name) + '</span>' +
-          '</div>' +
-          '<div>' +
-            '<span>' + (position) + '</span>' +
-          '</div>' +
-          '<div>' +
-            '<span>' + (number) + '</span>' +
-          '</div>' +
-        '</div>';
-            $(".player-card").prepend(".player-card");
-        });
